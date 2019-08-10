02dc81a05c01549e1af6609b11808b630ab99b50
Devavrat Raikar <37200755+rdevavrat@users.noreply.github.com>
2019-08-10 19:17:21 -0400

    Update tweet_mines.R

--- Part 3/Code/tweet_mines.R
+++ Part 3/Code/tweet_mines.R
@@ -17,24 +17,24 @@ library(dplyr)
 #setting up the authentication using secret keys and tokens
 token <- create_token(
   app = "ML Model",
-  consumer_key = "oRIjXvYi1qRAus5u2pC5jJFWx",
-  consumer_secret = "Z2LmQnNbikrT3xOaqK7tGfIiJaszywFstRvXaTPTs1wgMFuM8n",
-  access_token = "429752368-74kgIRzCGa7BrkmhHMOVs09yUVVK8Ch9lrZrB9dE",
-  access_secret = "H5qYfc4u2aMIRrUmHHcKLZtKD3SoVnYT4Bby0BQxSJhHy")
+  consumer_key = "",
+  consumer_secret = "",
+  access_token = "",
+  access_secret = "")
 identical(token, get_token())
 
 
 #mining tweets using different keywords
 tweets <- search_tweets("flu OR #flu OR FLU OR #FLU OR #Influenza OR Influenza OR #INFLUENZA OR INFLUENZA OR sneeze OR cold", 
                         n = 100000, lang="en", type="recent",
-                        geocode = lookup_coords("usa", apikey = "AIzaSyC9H_AJJtINiyw9L5g2PgSdF7tOc8kXXaA"), 
+                        geocode = lookup_coords("usa", apikey = ""), 
                         include_rts = FALSE, retryonratelimit=TRUE)
 tweets$geo_coords <- vapply(tweets$geo_coords, paste, collapse = ", ", character(1L))
 tweets$created_at <- vapply(tweets$created_at, paste, collapse = ", ", character(1L))
 write.csv(tweets[,c("text", "created_at", "screen_name", "geo_coords")], file="mined_twitter_data.csv", row.names=TRUE)
 tweets1 <- search_tweets("viral+infection OR congestion OR body+aches OR fever", 
                          n = 100000, lang="en", type="recent",
-                         geocode = lookup_coords("usa", apikey = "AIzaSyC9H_AJJtINiyw9L5g2PgSdF7tOc8kXXaA"), 
+                         geocode = lookup_coords("usa", apikey = ""), 
                          include_rts = FALSE, retryonratelimit=TRUE)
 #pre processing data to save it in the csv file.
 tweets1$geo_coords <- vapply(tweets1$geo_coords, paste, collapse = ", ", character(1L))
